361f6cc2fd4138b88e690139c85af57d3c7e4c97
xavier <xaviernaranjo1130@gmail.com>
2019-09-12 22:14:04 -0400

    basic idea

---
+++ main.py
@@ -0,0 +1,12 @@
+#Xavier Naranjo 09/12/2019
+#Description of program
+print("Welcome to, World of Warcraft Character Compatibility test!")
+print("This is where you will pick\nYour true embodiment of a WoW character\nBased off a few rounds of questioning.")
+name=input("First and foremost, what is your name?")
+print("welcome",name +" to your first encounter to the world of Azeroth!!")
+#first round of questions will be for horde or alliance
+#second round of questions will be for race\
+#third round of questions will be for class
+print("You are put in a position where you are stuck between\ngoing after purse robbers to help a women\n"
+      "or taking up a rather large amount of cash\n that was dropped beside you ")
+
