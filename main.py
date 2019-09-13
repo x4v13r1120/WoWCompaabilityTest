a28f7193af2a7f3fa396d68d74fc9ad90cd7f7a1
xavier <xaviernaranjo1130@gmail.com>
2019-09-13 13:19:41 -0400

    question 1 needs work but working on question 2

--- main.py
+++ main.py
@@ -1,29 +1,54 @@
-#Xavier Naranjo 09/12/2019
-#Description of program
+# Xavier Naranjo 09/12/2019
+# Description of program
 print("Welcome to, World of Warcraft Character Compatibility test!")
 print("")
 print("This is where you will pick\nYour true embodiment of a WoW character\nBased off a few rounds of questioning.")
 print("")
-name=input("First and foremost, what is your name?")
+name = input("First and foremost, what is your name?")
 print("")
-print("welcome",name +" to your first encounter to the world of Azeroth!!")
-#first round of questions will be for allegiance
-#second round of questions will be for race\
-#third round of questions will be for class
+print("welcome", name + " to your first encounter to the world of Azeroth!!")
+# first round of questions will be for allegiance
+# second round of questions will be for race\
+# third round of questions will be for class
 
 print("Question 1")
 print("")
 print("You are put in a position where you are stuck between\ngoing after purse robbers to help a women\n"
       "or taking up a rather large amount of cash\nthat was dropped beside you. ")
 print("")
-choice1=("A.help the women")
-choice2=("B.take the cash")
-print("Do you",choice1+" or",choice2)
+choice1 = ("A.help the women")
+choice2 = ("B.take the cash")
+print("Do you", choice1 + " or", choice2)
 print("")
-question1=input("A or  B.   ")
-A=("Alliance")
-B=("Horde")
+question1 = input("A or B?  ")
+A = choice1
+B = choice2
 print("")
 print("interesting choice lets move on")
+print("")
+
+pointA=1
+pointB=1
+
+pointTowardAlliance =pointA
+
+pointTowardHorde =pointB
+
+good = pointTowardAlliance
+
+bad = pointTowardHorde
+
+A = good
+
+B = bad
+
+Alliance=(pointA*3)
+
+Horde=(pointB*3)
+
+allegiance = "Horde" or "Alliance"
+
+
+print(allegiance)
 print("")
 print("Question 2")
