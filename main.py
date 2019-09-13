04746a87ff15443897928302e66bb2610b5d3225
xavier <xaviernaranjo1130@gmail.com>
2019-09-12 23:08:33 -0400

    question needs work but working on question 2

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 #Xavier Naranjo 09/12/2019
 #Description of program
 print("Welcome to, World of Warcraft Character Compatibility test!")
+print("")
 print("This is where you will pick\nYour true embodiment of a WoW character\nBased off a few rounds of questioning.")
+print("")
 name=input("First and foremost, what is your name?")
+print("")
 print("welcome",name +" to your first encounter to the world of Azeroth!!")
-#first round of questions will be for horde or alliance
+#first round of questions will be for allegiance
 #second round of questions will be for race\
 #third round of questions will be for class
-print("You are put in a position where you are stuck between\ngoing after purse robbers to help a women\n"
-      "or taking up a rather large amount of cash\n that was dropped beside you ")
 
+print("Question 1")
+print("")
+print("You are put in a position where you are stuck between\ngoing after purse robbers to help a women\n"
+      "or taking up a rather large amount of cash\nthat was dropped beside you. ")
+print("")
+choice1=("A.help the women")
+choice2=("B.take the cash")
+print("Do you",choice1+" or",choice2)
+print("")
+question1=input("A or  B.   ")
+A=("Alliance")
+B=("Horde")
+print("")
+print("interesting choice lets move on")
+print("")
+print("Question 2")
