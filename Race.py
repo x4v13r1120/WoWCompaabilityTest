4b8292dbe7e63cf46e83dd194d105e910884e7c4
xavier <xaviernaranjo1130@gmail.com>
2019-09-23 17:18:23 -0400

    Intro to 2nd round done will need and if and else statement in begin to see which classes they will be able to become

---
+++ Race.py
@@ -0,0 +1,23 @@
+#13 races and 12 classes
+#horde has 7 races and so does alliance
+#horde are stated in main aswell as allaince's
+
+#ALLIANCE RACES AND AVALIBLE CLASSES
+#human-warrior,paladin,hunter,rouge,mage,warlock,monk,priest,death knight
+#dwarf-warrior,paladin,hunter,rouge,shaman,mage,warlock,monk,priest,death knight
+#night elf-warrior,hunter,rouge,mage,monk,priest,druid,demon hunter,death knight
+#gnome
+#draenei
+#worgen
+
+#BOTH
+#pandaren
+
+
+#HORDE RACES AND AVALIBLE CLASSES
+#orc-
+#undead
+#tauren
+#troll
+#blood elf
+#goblin
